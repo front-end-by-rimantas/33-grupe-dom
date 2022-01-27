8cb66e92b3366d694de4c7dcbea191a13f3e157e
Rimantas <belauzas@yahoo.com>
2022-01-27 11:06:06 +0200

    DOM

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-for (let i = 0; i < 5; i++) {
-    console.log(i);
+const titleDOM = document.querySelector('h1');
+const listDOM = document.querySelector('ul');
+const darzovesDOM = document.querySelectorAll('ul > li > span');
+
+const title = titleDOM.innerText;
+const newTitle = title + ' yra apvalus';
+console.log(newTitle);
+
+const darzoviuPavadinimai = [];
+for (let i = 0; i < darzovesDOM.length; i++) {
+    const darzeveDOM = darzovesDOM[i];
+    darzoviuPavadinimai.push(darzeveDOM.innerText);
 }
+console.log(darzoviuPavadinimai);
+
+const darzoviuSuvestine = `Suvestine: ${darzoviuPavadinimai.join(', ')}.`;
+console.log(darzoviuSuvestine);
